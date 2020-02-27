aca9cfb9d25d109b4d96aa1fd16fcf5e47ce05b5
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-27 16:50:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,8 +5,10 @@ require 'pry'
 def nyc_pigeon_organizer(data)
 new = {}
 data.map do |key, value| 
-binding.pry
-  
+  value.map do |color, names| 
+    names.map do |name| 
+      new.key
+    end
+  end
 end
-
 end
